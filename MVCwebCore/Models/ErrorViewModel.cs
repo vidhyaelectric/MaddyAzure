b80f0ef90c3c3c0e9006b56231552e71ceeec6b3
Anand Narayanan <anandmadhavanforu@gmail.com>
2018-04-04 21:06:19 -0400

    test github

---
+++ MVCwebCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCwebCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
